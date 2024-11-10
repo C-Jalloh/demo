25bb734d54a9de03622630496983ae91001a2257
c_jalloh <esjallow03@gmail.com>
2024-11-10 16:23:33 +0000

    just more stuff

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class HelloController {
@@ -11,9 +16,17 @@ public class HelloController {
         this.videoService = videoService;
     }
    @GetMapping("/")
-public String index() {
-
+public String index(Model model) {
+     model.addAttribute("videos", videoService.getVideos());  // Passing the videos to the view.
     return "index";
 }
+
+@PostMapping("/new-video")
+public String newVideo(@ModelAttribute Video newVideo) {
+    //TODO: process POST request
+    videoService.createVideo(newVideo);
+    return "redirect:/";
+}
+
     
 }
--- src/main/java/com/example/demo/VideoService.java
+++ src/main/java/com/example/demo/VideoService.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -15,5 +16,12 @@ public class VideoService {
 
     public List<Video> getVideos(){
         return videos;
+    }
+
+    public Video createVideo(Video newVideo) {
+        List<Video> extend = new ArrayList<>(videos);
+        extend.add(newVideo);
+        this.videos = List.copyOf(extend);
+        return newVideo;
     }
 }
